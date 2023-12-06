bcd77ecb561b32277bd1d614cba36fced9bbc2e3
alanfacol <alanms.silva@unifacol.edu.br>
2023-12-06 16:38:22 -0300

    feat: add CORS

--- src/main/java/br/com/facol/livrariaback/LivrariaBackApplication.java
+++ src/main/java/br/com/facol/livrariaback/LivrariaBackApplication.java
@@ -2,9 +2,22 @@ package br.com.facol.livrariaback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@ServletComponentScan
+@Configuration
+@EnableWebMvc
 @SpringBootApplication
-public class LivrariaBackApplication {
+public class LivrariaBackApplication implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LivrariaBackApplication.class, args);
